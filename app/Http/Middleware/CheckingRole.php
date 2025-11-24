4d9fff5c6d8c73bc71c52dfbb1b6ee8b7a4b6b6f
idhammansyah <mansyahidham@gmail.com>
2025-11-24 14:45:01 +0700

    pembaruan di middleware

--- app/Http/Middleware/CheckingRole.php
+++ app/Http/Middleware/CheckingRole.php
@@ -9,17 +9,25 @@ use Symfony\Component\HttpFoundation\Response;
 
 class CheckingRole
 {
-  public function handle($request, Closure $next, $role)
-{
-    if (! Auth::check()) {
-      return redirect('/');
+
+  public function handle($request, Closure $next, ...$roles)
+  {
+    $user = Auth::user();
+
+    // Convert each role param like "1,2" into ["1","2"]
+    $parsedRoles = [];
+
+    foreach ($roles as $r) {
+        foreach (explode(',', $r) as $value) {
+            $parsedRoles[] = trim($value);
+        }
     }
 
-    if (Auth::user()->role_id != $role) {
-      abort(403, 'Unauthorized.');
+    if (!in_array($user->role_id, $parsedRoles)) {
+        abort(403, 'Unauthorized');
     }
 
     return $next($request);
-}
+  }
 
 }
